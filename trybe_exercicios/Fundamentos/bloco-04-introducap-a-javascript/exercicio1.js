9fa37759a8000634afbc658630fe1cac7158393d
Kennethy <kennethymov@gmail.com>
2022-02-02 16:28:41 -0300

    Exercicios 1,2,3 e 4 do bloco 4, dia 1

---
+++ trybe_exercicios/Fundamentos/bloco-04-introducap-a-javascript/exercicio1.js
@@ -0,0 +1,11 @@
+const myName = "Kennethy";
+const birthCity = "Belo Horizonte";
+let birthYear = 1991;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2000;
+
+console.log(birthYear);
